2bae80c487dda6ec4a02beb3e4427a15281a6045
Programming 1 <code@metrohomeschoolrobotics.org>
2020-01-20 20:53:44 -0600

    DriveSystem Upload

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2019 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -8,49 +8,91 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
 
+//import edu.wpi.first.wpilibj.CameraServer;
+//import edu.wpi.first.cameraserver.CameraServer;
+//import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+//import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+//import frc.robot.commands.*;
+import frc.robot.subsystems.*;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+//import edu.wpi.first.wpilibj.Talon;
+//import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+//import com.ctre.phoenix.motorcontrol.InvertType;
+//import com.ctre.phoenix.motorcontrol.can.*;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 /**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the TimedRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.gradle file in the
  * project.
  */
 public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
+  public static OI m_oi;
+  public static OctaDrive m_octaDrive;
 
-  private RobotContainer m_robotContainer;
+  Command m_autonomousCommand;
+
+  //SendableChooser<Command> m_chooser = new SendableChooser<>();
 
   /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
+   * This function is run when the robot is first started up and should be
+   * used for any initialization code.
    */
   @Override
   public void robotInit() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-    // autonomous chooser on the dashboard.
-    m_robotContainer = new RobotContainer();
+    //CameraServer.getInstance().startAutomaticCapture();
+    DriveSystemBase tankDrive = new TankDrive(
+      new CANSparkMax(RobotMap.LeftFrontMotor, MotorType.kBrushless), 
+      new CANSparkMax(RobotMap.RightFrontMotor, MotorType.kBrushless),
+      new CANSparkMax(RobotMap.LeftRearMotor, MotorType.kBrushless),
+      new CANSparkMax(RobotMap.RightRearMotor, MotorType.kBrushless));
+    DriveSystemBase mechDrive = new MechDrive(
+        new CANSparkMax(RobotMap.LeftFrontMotor, MotorType.kBrushless), 
+        new CANSparkMax(RobotMap.RightFrontMotor, MotorType.kBrushless),
+        new CANSparkMax(RobotMap.LeftRearMotor, MotorType.kBrushless),
+        new CANSparkMax(RobotMap.RightRearMotor, MotorType.kBrushless));
+
+/*        DriveSystemBase tankDrive = new TankDrive(
+          new TalonSRX(RobotMap.LeftFrontMotor),
+          new TalonSRX(RobotMap.RightFrontMotor),
+          new TalonSRX(RobotMap.LeftRearMotor),
+          new TalonSRX(RobotMap.RightRearMotor));
+        DriveSystemBase mechDrive = new MechDrive(
+          new TalonSRX(RobotMap.LeftFrontMotor),
+          new TalonSRX(RobotMap.RightFrontMotor),
+          new TalonSRX(RobotMap.LeftRearMotor),
+          new TalonSRX(RobotMap.RightRearMotor));*/
+
+    m_octaDrive = new OctaDrive(
+      tankDrive, 
+      mechDrive,
+      new DoubleSolenoid(RobotMap.TankSwitchForward, RobotMap.TankSwitchReverse));
+
+    m_oi = new OI(tankDrive, m_octaDrive, mechDrive);
+    m_oi.init();
   }
 
   /**
-   * This function is called every robot packet, no matter the mode. Use this for items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
+   * This function is called every robot packet, no matter the mode. Use
+   * this for items like diagnostics that you want ran during disabled,
+   * autonomous, teleoperated and test.
    *
    * <p>This runs after the mode specific periodic functions, but before
    * LiveWindow and SmartDashboard integrated updating.
    */
   @Override
   public void robotPeriodic() {
-    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-    // commands, running already-scheduled commands, removing finished or interrupted commands,
-    // and running subsystem periodic() methods.  This must be called from the robot's periodic
-    // block in order for anything in the Command-based framework to work.
-    CommandScheduler.getInstance().run();
   }
 
   /**
    * This function is called once each time the robot enters Disabled mode.
+   * You can use it to reset any subsystem information you want to clear when
+   * the robot is disabled.
    */
   @Override
   public void disabledInit() {
@@ -58,18 +100,38 @@ public class Robot extends TimedRobot {
 
   @Override
   public void disabledPeriodic() {
+    Scheduler.getInstance().run();
+    m_oi._driveTank.cancel();
+    //m_oi._driveOcta.cancel();
   }
 
   /**
-   * This autonomous runs the autonomous command selected by your {@link RobotContainer} class.
+   * This autonomous (along with the chooser code above) shows how to select
+   * between different autonomous modes using the dashboard. The sendable
+   * chooser code works with the Java SmartDashboard. If you prefer the
+   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+   * getString code to get the auto name from the text box below the Gyro
+   *
+   * <p>You can add additional auto modes by adding additional commands to the
+   * chooser code above (like the commented example) or additional comparisons
+   * to the switch structure below with additional strings & commands.
    */
   @Override
   public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+    //m_autonomousCommand = m_chooser.getSelected();
+    m_autonomousCommand = m_oi.getAutonomousCommand();
+    m_oi._driveTank.cancel();
+    //m_oi._driveOcta.cancel();
+    /*
+     * String autoSelected = SmartDashboard.getString("Auto Selector",
+     * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
+     * = new MyAutoCommand(); break; case "Default Auto": default:
+     * autonomousCommand = new ExampleCommand(); break; }
+     */
 
     // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
+      m_autonomousCommand.start();
     }
   }
 
@@ -78,6 +140,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousPeriodic() {
+    Scheduler.getInstance().run();
   }
 
   @Override
@@ -89,6 +152,8 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
+    m_oi._driveTank.start();
+    //m_oi._driveOcta.start();
   }
 
   /**
@@ -96,12 +161,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void teleopPeriodic() {
-  }
-
-  @Override
-  public void testInit() {
-    // Cancels all running commands at the start of test mode.
-    CommandScheduler.getInstance().cancelAll();
+    Scheduler.getInstance().run();
   }
 
   /**
